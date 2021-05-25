76426bfc9df39fe4e9792920be2baed09118ad0a
liuxingxing23 <liuxingxing23@jd.com>
2021-05-25 20:40:44 +0800

    add: 添加基本的生产消费api

---
+++ src/main/java/com/icecola/simplemq/api/consumer/SimpleConsumerImpl.java
@@ -0,0 +1,20 @@
+package com.icecola.simplemq.api.consumer;
+
+import com.icecola.simplemq.api.IConsumer;
+import com.icecola.simplemq.queue.Message;
+
+import java.util.List;
+
+/**
+ * description:
+ *
+ * @author liuxingxing23@jd.com
+ * @date 2021/5/25 20:30
+ */
+public class SimpleConsumerImpl implements IConsumer {
+
+    @Override
+    public void onMessages(List<Message> messages) {
+
+    }
+}
---
+++ src/main/java/com/icecola/simplemq/api/provider/SimpleProviderImpl.java
@@ -0,0 +1,18 @@
+package com.icecola.simplemq.api.provider;
+
+import com.icecola.simplemq.api.IProvider;
+import com.icecola.simplemq.queue.Message;
+
+/**
+ * description:
+ *
+ * @author liuxingxing23@jd.com
+ * @date 2021/5/25 20:31
+ */
+public class SimpleProviderImpl implements IProvider {
+
+    @Override
+    public void send(Message message) {
+
+    }
+}
